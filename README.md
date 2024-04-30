7f4f157e84bef6967c8d47af59d72a33a58539ec
Ricardo Morin <morinricardo@gmail.com>
2024-04-30 14:42:13 -0700

    Upgrade node runtime, replace old config parms with env vars

--- README.md
+++ README.md
@@ -1,30 +1,25 @@
 # addToMailgunList
 
-This Google Cloud Function is used to add new members to an arbitrary mailgun mailing list, depending on runtime configuration parameters.
-
-## Configuration parameters
-```
-gcloud beta runtime-config configs create hm-mailinglistparms
-```
+This Google Cloud Function is used to add new members to an arbitrary mailgun mailing list, depending on runtime configuration parameters set in environment variables.
 
 ### Mailing List
 ```
-gcloud beta runtime-config configs variables set mailinglist "maymailinglist@mydomain.com" --config-name hm-mailinglistparms
+MAILINGLIST
 ```
 
 ### Mailgun API Key
 ```
-gcloud beta runtime-config configs variables set mailgun-api-key "key-XXXXXXXXXXXXXXXXXXXXXXX" --config-name hm-mailinglistparms
+MAILGUNAPIKEY
 ```
 
 ### Mailgun Domain
 ```
-gcloud beta runtime-config configs variables set mailgun-domain "mydomain.com" --config-name hm-mailinglistparms
+MAILGUNDOMAIN
 ```
 
 ### Email to send notifications
 ```
-gcloud beta runtime-config configs variables set notification-email "myemail@mydomain.com" --config-name hm-mailinglistparms
+NOTIFICATIONEMAIL
 ```
 
 ## Deploy
--- index.js
+++ index.js
@@ -1,47 +1,34 @@
-
-const CORS = require('cors')({origin: '*'}); // API must be accessible
 const MAILGUN = require('mailgun-js');
-
-// Get configuration variables
-const CONFIG = require('cloud-functions-runtime-config').getVariables('hm-mailinglistparms', ['mailgun-domain', 'mailgun-api-key', 'mailinglist', 'notification-email']);
 
 /*
   This Google Cloud Function is used to add new members to an arbitrary mailgun mailing list,
-  depending on runtime config parameters.
+  depending on runtime config parameters set in environment variables.
 */
 exports.addToMailgunList = (req, res) => {
-  CORS(req, res, () => {
-    if (req.method === 'POST') {
-      CONFIG.then((values) => {
-        var mailgunDomain = values[0];
-        var mailgunApiKey = values[1];
-        var mailinglist = values[2];
-        var notificationEmail = values[3];
-        var mailgun = new MAILGUN({apiKey: mailgunApiKey, domain: mailgunDomain});
-        var list = mailgun.lists(mailinglist);
-        var member = {
-          subscribed: true,
-          address: req.body.email
-        };
-        list.members().create(member, function (err, data) {
-          if (err) {
-            res.status(err.statusCode).send(data);
-          } else {
-            res.send(data);
-            // Notify with an email -- no big deal if it fails
-            mailgun.messages().send({
-              from: 'Mailing list request<' + 'postmaster@' + mailgunDomain + '>',
-              to: notificationEmail,
-              subject: 'FYI: new address added to mailing list',
-              text: req.body.email + ' has been added to the mailing list ' + mailinglist
-            });
-          }
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Methods', 'POST');
+  if (req.method === 'POST') {
+    var mailgun = new MAILGUN({apiKey: process.env.MAILGUNAPIKEY, domain: process.env.MAILGUNDOMAIN});
+    var list = mailgun.lists(process.env.MAILINGLIST);
+    var member = {
+      subscribed: true,
+      address: req.body.email
+    };
+    list.members().create(member, function (err, data) {
+      if (err) {
+        res.status(err.statusCode).send(data);
+      } else {
+        res.send(data);
+        // Notify with an email -- no big deal if it fails
+        mailgun.messages().send({
+          from: 'Mailing list request<' + 'postmaster@' + process.env.MAILGUNDOMAIN + '>',
+          to: process.env.NOTIFICATIONEMAIL,
+          subject: 'FYI: new address added to mailing list',
+          text: req.body.email + ' has been added to the mailing list ' + process.env.MAILINGLIST
         });
-      }).catch((err) => {
-        res.status(500).send(err);
-      });
-    } else {
-      res.status(405).send({message: 'Only POST method is allowed'});
-    }
-  });
+      }
+    });
+  } else {
+    res.status(405).send({message: 'Only POST method is allowed'});
+  }
 };
